be053847758d3fab03b3504ab9825ed855b7dff2
Shogun <ShogunPanda@users.noreply.github.com>
2022-02-14 11:22:46 +0100

    chore: Removed useless directory.

--- scripts/logs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set -x 
-aws logs get-log-events --region $AWS_REGION --log-group-name /aws/lambda/$LAMBDA --log-stream-name $@ | jq -r -M -c '.events[].message | tostring | sub("\\s+$"; "")'
