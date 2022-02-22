f7ab248d68dc8c19cdb0c1a79b3d7532d245039b
Paolo Insogna <paolo@cowtech.it>
2022-02-22 13:00:44 +0100

    feat: Added telemetry. (#8)

--- src/storage.js
+++ src/storage.js
@@ -6,6 +6,7 @@ const { NodeHttpHandler } = require('@aws-sdk/node-http-handler')
 const { Agent } = require('https')
 
 const { logger, serializeError } = require('./logging')
+const { metrics, trackDuration } = require('./telemetry')
 
 const agent = new Agent({ keepAlive: true, keepAliveMsecs: 60000 })
 
@@ -19,8 +20,13 @@ const s3Client = new S3Client({
 
 async function fetchFromS3(bucket, key) {
   try {
+    metrics.s3Fetchs.add(1)
+
     // Download from S3
-    const record = await s3Client.send(new GetObjectCommand({ Bucket: bucket, Key: key }))
+    const record = await trackDuration(
+      metrics.s3FetchsDurations,
+      s3Client.send(new GetObjectCommand({ Bucket: bucket, Key: key }))
+    )
     const buffer = new BufferList()
 
     for await (const chunk of record.Body) {
@@ -36,8 +42,11 @@ async function fetchFromS3(bucket, key) {
 
 async function uploadToS3(bucket, key, content) {
   try {
-    await s3Client.send(
-      new PutObjectCommand({ Bucket: bucket, Key: key, Body: content, ContentType: 'application/json' })
+    metrics.s3Uploads.add(1)
+
+    await trackDuration(
+      metrics.s3UploadsDurations,
+      s3Client.send(new PutObjectCommand({ Bucket: bucket, Key: key, Body: content, ContentType: 'application/json' }))
     )
   } catch (e) {
     logger.error(`Cannot upload file ${key} to S3 bucket ${bucket}: ${serializeError(e)}`)
@@ -48,7 +57,12 @@ async function uploadToS3(bucket, key, content) {
 
 async function publishToSQS(queue, data, additionalAttributes = {}) {
   try {
-    await sqsClient.send(new SendMessageCommand({ QueueUrl: queue, MessageBody: data, ...additionalAttributes }))
+    metrics.sqsPublishes.add(1)
+
+    await trackDuration(
+      metrics.sqsPublishesDurations,
+      sqsClient.send(new SendMessageCommand({ QueueUrl: queue, MessageBody: data, ...additionalAttributes }))
+    )
   } catch (e) {
     logger.error(`Cannot publish a block to ${queue}: ${serializeError(e)}`)
 
---
+++ src/telemetry.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus')
+const { MeterProvider } = require('@opentelemetry/sdk-metrics-base')
+
+const exporter = new PrometheusExporter({ preventServerStart: true })
+const meters = {}
+const metrics = {}
+
+// Create all the metrics
+meters.s3Meter = new MeterProvider({ exporter, interval: 100 }).getMeter('s3')
+metrics.s3Fetchs = meters.s3Meter.createCounter('s3-fetchs', { description: 'Fetchs on S3' })
+metrics.s3FetchsDurations = meters.s3Meter.createCounter('s3-fetchs-durations', {
+  description: 'Fetchs durations on S3'
+})
+metrics.s3Uploads = meters.s3Meter.createCounter('s3-uploads', { description: 'Uploads on S3' })
+metrics.s3UploadsDurations = meters.s3Meter.createCounter('s3-uploads-durations', {
+  description: 'Uploads durations on S3'
+})
+
+meters.sqsMeter = new MeterProvider({ exporter, interval: 100 }).getMeter('sqs')
+metrics.sqsPublishes = meters.sqsMeter.createCounter('sqs-publishes', { description: 'Publishes on SQS' })
+metrics.sqsPublishesDurations = meters.sqsMeter.createCounter('sqs-publishes-durations', {
+  description: 'Publishes durations on SQS'
+})
+
+meters.httpMeter = new MeterProvider({ exporter, interval: 100 }).getMeter('http')
+metrics.httpFetchHeadCid = meters.httpMeter.createCounter('http-fetchs-head-id', { description: 'Fetchs of head CID' })
+metrics.httpFetchHeadCidDurations = meters.httpMeter.createCounter('http-fetch-head-id-durations', {
+  description: 'Fetchs duration of head CID'
+})
+metrics.indexerNotification = meters.httpMeter.createCounter('http-indexer-notifications', {
+  description: 'Notifications of a new head to the Indexer'
+})
+metrics.indexerNotificationDurations = meters.httpMeter.createCounter('http-indexer-notifications-durations', {
+  description: 'Notifications durations of a new head to the Indexer'
+})
+
+async function trackDuration(metric, promise) {
+  const startTime = process.hrtime.bigint()
+
+  try {
+    return await promise
+  } finally {
+    metric.add(Number(process.hrtime.bigint() - startTime) / 1e6)
+  }
+}
+
+function storeMetrics() {
+  /* c8 ignore next 3 */
+  if (!exporter._batcher.hasMetric) {
+    return '# no registered metrics'
+  } else {
+    return exporter._serializer.serialize(exporter._batcher.checkPointSet())
+  }
+}
+
+module.exports = {
+  meters,
+  metrics,
+  storeMetrics,
+  trackDuration
+}
