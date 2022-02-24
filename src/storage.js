5e9091a89af337c7e7978aef85c5083046bbb401
Shogun <paolo@cowtech.it>
2022-02-24 11:53:14 +0100

    feat: Vastly improve telemetry.

--- src/storage.js
+++ src/storage.js
@@ -1,12 +1,11 @@
 const { BufferList } = require('bl')
 const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3')
 const { SQSClient, SendMessageCommand } = require('@aws-sdk/client-sqs')
-
 const { NodeHttpHandler } = require('@aws-sdk/node-http-handler')
 const { Agent } = require('https')
 
 const { logger, serializeError } = require('./logging')
-const { metrics, trackDuration } = require('./telemetry')
+const telemetry = require('./telemetry')
 
 const agent = new Agent({ keepAlive: true, keepAliveMsecs: 60000 })
 
@@ -20,11 +19,11 @@ const s3Client = new S3Client({
 
 async function fetchFromS3(bucket, key) {
   try {
-    metrics.s3Fetchs.add(1)
+    telemetry.increaseCount('s3-fetchs')
 
     // Download from S3
-    const record = await trackDuration(
-      metrics.s3FetchsDurations,
+    const record = await telemetry.trackDuration(
+      's3-fetchs',
       s3Client.send(new GetObjectCommand({ Bucket: bucket, Key: key }))
     )
     const buffer = new BufferList()
@@ -42,10 +41,10 @@ async function fetchFromS3(bucket, key) {
 
 async function uploadToS3(bucket, key, content) {
   try {
-    metrics.s3Uploads.add(1)
+    telemetry.increaseCount('s3-uploads')
 
-    await trackDuration(
-      metrics.s3UploadsDurations,
+    await telemetry.trackDuration(
+      's3-uploads',
       s3Client.send(new PutObjectCommand({ Bucket: bucket, Key: key, Body: content, ContentType: 'application/json' }))
     )
   } catch (e) {
@@ -57,10 +56,10 @@ async function uploadToS3(bucket, key, content) {
 
 async function publishToSQS(queue, data, additionalAttributes = {}) {
   try {
-    metrics.sqsPublishes.add(1)
+    telemetry.increaseCount('sqs-publishes')
 
-    await trackDuration(
-      metrics.sqsPublishesDurations,
+    await telemetry.trackDuration(
+      'sqs-publishes',
       sqsClient.send(new SendMessageCommand({ QueueUrl: queue, MessageBody: data, ...additionalAttributes }))
     )
   } catch (e) {
--- test/miscellaneous.test.js
+++ test/miscellaneous.test.js
@@ -9,6 +9,8 @@ const { createFromJSON } = require('peer-id')
 const { Readable } = require('stream')
 const t = require('tap')
 const { getPeerId } = require('../src/config')
+const { logger } = require('../src/logging')
+const telemetry = require('../src/telemetry')
 const { s3Mock } = require('./utils/mock')
 
 t.test('config - download the peerId from S3', async t => {
@@ -39,4 +41,58 @@ t.test('config - creates a new PeerId if download fails', async t => {
   })
 
   t.not((await getPeerId()).toB58String(), (await createFromJSON(JSON.parse(rawPeer))).toB58String())
+})
+
+t.test('telemetry - correctly implements interfaces', async t => {
+  t.plan(5)
+
+  // Reset other metrics
+  telemetry.logger = {
+    info(arg) {}
+  }
+  await telemetry.flush()
+
+  telemetry.createMetric('custom', 'Custom', 'count', 'createUpDownCounter')
+
+  // Set the logger to check the tracking
+  telemetry.logger = {
+    info(arg) {
+      t.strictSame(arg, {
+        ipfs_provider_component: 'publisher-lambda',
+        metrics: { 's3-fetchs-count': 0, 's3-fetchs-durations': [], 'custom-count': 1 }
+      })
+    }
+  }
+
+  telemetry.increaseCount('custom')
+  telemetry.increaseCount('custom')
+  telemetry.decreaseCount('custom')
+  await telemetry.flush()
+
+  // Set the logger to check the refresh
+  telemetry.logger = {
+    info(arg) {
+      t.strictSame(arg, {
+        ipfs_provider_component: 'publisher-lambda',
+        metrics: { 's3-fetchs-count': 0, 's3-fetchs-durations': [], 'custom-count': 0 }
+      })
+    }
+  }
+
+  await telemetry.flush()
+
+  // Now check other methods
+  telemetry.logger = {
+    info(arg) {}
+  }
+
+  t.throws(() => telemetry.decreaseCount('unknown'), 'Metrics unknown not found.')
+  await t.resolves(() => telemetry.shutdown())
+
+  telemetry.export([], argument => {
+    t.equal(argument, 'SUCCESS')
+  })
+
+  // Reset the logger
+  telemetry.logger = logger
 })
